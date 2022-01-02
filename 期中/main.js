ed9189378142b96e19264ca989dc4962d3aee024
qwezxca123 <79677968+qwezxca123@users.noreply.github.com>
2022-01-02 18:01:11 +0800

    Update main.js

--- 期中/main.js
+++ 期中/main.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.60.0/http/server.ts"
 const s = serve({ port: 8533 })
-console.log("http://localhost:8533/canvas.html")
+console.log("http://172.104.100.202:8533/canvas.html")
 for await (const req of s) {
   console.log('req.url=', req.url)
   try {
